af92f304b97507a29b1c13143643f5a01eda184e
Nora C <nocorser@gmail.com>
2021-06-11 11:59:51 -0500

    logger() working

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,39 @@
 function logger(req, res, next) {
-  // DO YOUR MAGIC
+  // logger() logs to the console the following information about each request:
+  // request method, request url, and a timestamp
+  // runs on every request made to the API
+  const timeFormat = {
+    weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'
+  };
+  const timestamp = new Date().toLocaleString('en-UK', timeFormat)  
+  const method = req.method
+  const url = req.originalUrl
+  console.log(`[${timestamp}]: ${method} request to ${url}`)
+  next()
 }
 
 function validateUserId(req, res, next) {
   // DO YOUR MAGIC
+  console.log('validateUserId middleware')
+  next()
 }
 
 function validateUser(req, res, next) {
   // DO YOUR MAGIC
+  console.log('validateUser middleware')
+  next()
 }
 
 function validatePost(req, res, next) {
   // DO YOUR MAGIC
+    console.log('validatePost middleware')
+    next()
 }
 
 // do not forget to expose these functions to other modules
+module.exports = {
+  logger,
+  validateUserId,
+  validateUser,
+  validatePost
+}
--- api/server.js
+++ api/server.js
@@ -1,9 +1,14 @@
+// imports
 const express = require('express');
+const { logger } = require('./middleware/middleware');
 
 const server = express();
 
 // remember express by default cannot parse JSON in request bodies
 server.use(express.json());
+
+// middleware
+server.use(logger);
 
 // global middlewares and the user's router need to be connected here
 
