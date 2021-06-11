677a1b93f596eacb514c07706a99b3827edd4135
Nora C <nocorser@gmail.com>
2021-06-11 16:05:58 -0500

    posts successfully to /:id/posts. woohoo

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -64,12 +64,15 @@ router.get('/:id/posts', validateUserId, async (req, res, next) => {
   }
 });
 
-router.post('/:id/posts', validateUserId, validatePost, (req, res) => {
+router.post('/:id/posts', validateUserId, validatePost, async (req, res, next) => {
   // RETURN THE NEWLY CREATED USER POST
-  // this needs a middleware to verify user id
-  // and another middleware to check that the request body is valid
-  console.log(req.user)
-  console.log(req.text)
+  // verify user id & check that post is valid
+  try {
+    const result = await Post.insert({
+      user_id: req.params.id, text: req.text
+    })
+    res.status(201).json(result)
+  } catch(err){next(err)}
 });
 
 // Gabe's error-handling middleware
